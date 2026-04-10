0c7ef0c9d68d9e4eefb64b9f361c85b8c3f71ac7
Tobias Heierli <heiertob@students.zhaw.ch>
2026-04-10 11:26:24 +0200

    PORTAMI-26: Feedback

--- src/components/NavigationLayout.tsx
+++ src/components/NavigationLayout.tsx
@@ -8,6 +8,7 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import {AppBar, Box, Button, Container, Drawer, IconButton, List, ListItem, Toolbar, Typography} from '@mui/material';
 import {NavLink, Outlet} from 'react-router';
+import logo from '@/assets/logo.svg';
 
 const navigationWidth = 240;
 
@@ -17,10 +18,10 @@ export default function NavigationLayout() {
             <AppBar position={'fixed'} sx={{zIndex: (theme) => theme.zIndex.drawer + 1}}>
                 <Toolbar>
                     <Box component={NavLink} to={'/'} sx={{display: 'inline-flex', lineHeight: 0}}>
-                        <Box component={'img'} src={'/src/assets/logo.svg'} alt={'Logo'} sx={{height: (theme) => theme.mixins.toolbar.minHeight}} />
+                        <Box component={'img'} src={logo} alt={'Logo'} sx={{height: (theme) => theme.mixins.toolbar.minHeight}} />
                     </Box>
                     <Box sx={{flexGrow: 1}} />
-                    <IconButton size={'large'} color={'inherit'}>
+                    <IconButton size={'large'}>
                         <NotificationsNoneIcon />
                     </IconButton>
                 </Toolbar>
@@ -41,31 +42,31 @@ export default function NavigationLayout() {
                 </Typography>
                 <List component={'nav'}>
                     <ListItem disableGutters>
-                        <Button component={NavLink} to={'/rollen'} startIcon={<Inventory2OutlinedIcon />} sx={{justifyContent: 'flex-start'}}>
+                        <Button component={NavLink} to={'/rollen'} startIcon={<Inventory2OutlinedIcon />} sx={{justifyContent: 'flex-start', width: '100%'}} >
                             Rollen
                         </Button>
                     </ListItem>
                     <ListItem disableGutters>
-                        <Button component={NavLink} to={'/inventar'} startIcon={<ContentPasteOutlinedIcon />} sx={{justifyContent: 'flex-start'}}>
+                        <Button component={NavLink} to={'/inventar'} startIcon={<ContentPasteOutlinedIcon />} sx={{justifyContent: 'flex-start', width: '100%'}}>
                             Inventar
                         </Button>
                     </ListItem>
                     <ListItem disableGutters>
-                        <Button component={NavLink} to={'/lager'} startIcon={<HomeOutlinedIcon />} sx={{justifyContent: 'flex-start'}}>
+                        <Button component={NavLink} to={'/lager'} startIcon={<HomeOutlinedIcon />} sx={{justifyContent: 'flex-start', width: '100%'}}>
                             Lager
                         </Button>
                     </ListItem>
                     <ListItem disableGutters>
-                        <Button component={NavLink} to={'/shopping'} startIcon={<ShoppingCartOutlinedIcon />} sx={{justifyContent: 'flex-start'}}>
+                        <Button component={NavLink} to={'/shopping'} startIcon={<ShoppingCartOutlinedIcon />} sx={{justifyContent: 'flex-start', width: '100%'}}>
                             Shopping
                         </Button>
                     </ListItem>
                 </List>
                 <Box sx={{flexGrow: 1}} />
-                <Button startIcon={<SettingsIcon />} sx={{justifyContent: 'flex-start'}}>
+                <Button startIcon={<SettingsIcon />} sx={{justifyContent: 'flex-start', width: '100%'}}>
                     Settings
                 </Button>
-                <Button startIcon={<LogoutIcon />} sx={{mt: 2, justifyContent: 'flex-start'}}>
+                <Button startIcon={<LogoutIcon />} sx={{mt: 2, justifyContent: 'flex-start', width: '100%'}}>
                     Logout
                 </Button>
             </Drawer>
