40c0ec488be7b9443470becfc82b99ef1eb536ce
SKY-Tobi <55585410+SKY-Tobi@users.noreply.github.com>
2026-04-11 10:20:15 +0200

    PORTAMI-26: Base layout for the application created together with some routing setup. (#13)
    * Base layout for the application created together with some routing setup.
    
    * PORTAMI-26: Logo switched with svg
    
    * PORTAMI-26: Feedback
    
    * PORTAMI-26: Formatting
    
    * PORTAMI-26: vite config
    
    ---------
    
    Co-authored-by: Maximilian Hubrath <Maximilian.hubrath@gmail.com>

---
+++ src/components/NavigationLayout.tsx
@@ -0,0 +1,89 @@
+import logo from '@/assets/logo.svg';
+import AddIcon from '@mui/icons-material/Add';
+import ContentPasteOutlinedIcon from '@mui/icons-material/ContentPasteOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
+import LogoutIcon from '@mui/icons-material/Logout';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import SettingsIcon from '@mui/icons-material/Settings';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import {AppBar, Box, Button, Container, Drawer, IconButton, List, ListItem, Toolbar, Typography} from '@mui/material';
+import {NavLink, Outlet} from 'react-router';
+
+const navigationWidth = 240;
+
+export default function NavigationLayout() {
+    return (
+        <Box sx={{display: 'flex'}}>
+            <AppBar position={'fixed'} sx={{zIndex: (theme) => theme.zIndex.drawer + 1}}>
+                <Toolbar>
+                    <Box component={NavLink} to={'/'} sx={{display: 'inline-flex', lineHeight: 0}}>
+                        <Box component={'img'} src={logo} alt={'Logo'} sx={{height: (theme) => theme.mixins.toolbar.minHeight}} />
+                    </Box>
+                    <Box sx={{flexGrow: 1}} />
+                    <IconButton size={'large'}>
+                        <NotificationsNoneIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                variant={'permanent'}
+                sx={{
+                    width: navigationWidth,
+                    [`& .MuiDrawer-paper`]: {width: navigationWidth, p: 4},
+                }}
+            >
+                <Toolbar />
+                <Button variant={'contained'} startIcon={<AddIcon />}>
+                    Filter
+                </Button>
+                <Typography variant={'subtitle2'} sx={{mt: 4}}>
+                    Shop
+                </Typography>
+                <List component={'nav'}>
+                    <ListItem disableGutters>
+                        <Button component={NavLink} to={'/rollen'} startIcon={<Inventory2OutlinedIcon />} sx={{justifyContent: 'flex-start', width: '100%'}}>
+                            Rollen
+                        </Button>
+                    </ListItem>
+                    <ListItem disableGutters>
+                        <Button
+                            component={NavLink}
+                            to={'/inventar'}
+                            startIcon={<ContentPasteOutlinedIcon />}
+                            sx={{justifyContent: 'flex-start', width: '100%'}}
+                        >
+                            Inventar
+                        </Button>
+                    </ListItem>
+                    <ListItem disableGutters>
+                        <Button component={NavLink} to={'/lager'} startIcon={<HomeOutlinedIcon />} sx={{justifyContent: 'flex-start', width: '100%'}}>
+                            Lager
+                        </Button>
+                    </ListItem>
+                    <ListItem disableGutters>
+                        <Button
+                            component={NavLink}
+                            to={'/shopping'}
+                            startIcon={<ShoppingCartOutlinedIcon />}
+                            sx={{justifyContent: 'flex-start', width: '100%'}}
+                        >
+                            Shopping
+                        </Button>
+                    </ListItem>
+                </List>
+                <Box sx={{flexGrow: 1}} />
+                <Button startIcon={<SettingsIcon />} sx={{justifyContent: 'flex-start', width: '100%'}}>
+                    Settings
+                </Button>
+                <Button startIcon={<LogoutIcon />} sx={{mt: 2, justifyContent: 'flex-start', width: '100%'}}>
+                    Logout
+                </Button>
+            </Drawer>
+            <Container>
+                <Toolbar />
+                <Outlet />
+            </Container>
+        </Box>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
 import './styles/index.scss';
 import App from './App';
 import theme from './theme';
+import NavigationLayout from '@/components/NavigationLayout.tsx';
+import Demo from '@/pages/Demo.tsx';
 import {CssBaseline, ThemeProvider} from '@mui/material';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Route, Routes} from 'react-router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
+        <BrowserRouter>
+            <CssBaseline />
+            <Routes>
+                <Route element={<NavigationLayout />}>
+                    <Route index element={<App />} />
+                </Route>
+                <Route path={'/demo'} element={<Demo />} />
+            </Routes>
+        </BrowserRouter>
     </ThemeProvider>,
 );
